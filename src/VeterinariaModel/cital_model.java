f4d166369bd3da716a2b76688fd30ac589f8efe5
Josue01 <josue.telenchana3175@utc.edu.ec>
2026-06-19 15:33:17 -0500

    /Cital_model

--- src/VeterinariaModel/cital_model.java
+++ src/VeterinariaModel/cital_model.java
@@ -8,13 +8,21 @@ package VeterinariaModel;
  *
  * @author el_ingeniero
  */
+package VeterinariaModel;
+public class cita_model {
+    public int codigo;
+    public int codigoMascota;
+    public int codigoVeterinario;
+    public String fecha;
+    public String motivo;
 public class cital_model {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    //CONSTRUCTOR
+ 
+    public cita_model(int codigo, int codigoMascota, int codigoVeterinario, String fecha, String motivo){
+        this.codigo= codigo;
+        this.codigoMascota= codigoMascota;
+        this.codigoVeterinario= codigoVeterinario;
+        this.fecha= fecha;
+        this.motivo= motivo;
     }
-    
-}
