d39f00a61c5710db81fc99b9ddacc7146b0655f5
Jairo <jean.serna2730@utc.edu.ec>
2026-06-19 13:43:42 -0500

    veterinarios

--- src/Veterinaria_view/veterinaria_view.java
+++ src/Veterinaria_view/veterinaria_view.java
@@ -47,3 +47,161 @@ public class veterinaria_view {
             }
         }
     }
+
+    // ---------------- SUBMENU MASCOTAS ----------------
+    public void menuMascotas() {
+        int opcion = -1;
+        while (opcion != 5) {
+            System.out.println("""
+                               -------------------------------------------------
+                                                MASCOTAS
+                               1. AGREGAR
+                               2. MOSTRAR
+                               3. ACTUALIZAR
+                               4. ELIMINAR
+                               5. VOLVER
+                               SELECCIONE LA OPCION A REALIZAR:
+                               """);
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingresa el codigo: ");
+                    int codigo = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Nombre de la mascota: ");
+                    String nombre = sc.nextLine();
+                    System.out.print("Especie: ");
+                    String especie = sc.nextLine();
+                    System.out.print("Raza: ");
+                    String raza = sc.nextLine();
+                    System.out.print("Edad: ");
+                    int edad = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Nombre del dueño: ");
+                    String dueño = sc.nextLine();
+
+                    mascota_model mascota = new mascota_model(codigo, nombre, especie, raza, edad, dueño);
+                    controlador.agregarMascota(mascota);
+                    break;
+
+                case 2:
+                    controlador.listarMascotas();
+                    break;
+
+                case 3:
+                    System.out.print("Codigo de la mascota a actualizar: ");
+                    int codigoAct = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Nuevo nombre: ");
+                    String nombreAct = sc.nextLine();
+                    System.out.print("Nueva especie: ");
+                    String especieAct = sc.nextLine();
+                    System.out.print("Nueva raza: ");
+                    String razaAct = sc.nextLine();
+                    System.out.print("Nueva edad: ");
+                    int edadAct = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Nuevo dueño: ");
+                    String dueñoAct = sc.nextLine();
+
+                    if (controlador.actualizarMascota(codigoAct, nombreAct, especieAct, razaAct, edadAct, dueñoAct)) {
+                        System.out.println("Mascota actualizada exitosamente!");
+                    } else {
+                        System.out.println("Mascota no encontrada!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Ingrese el codigo de la mascota a eliminar: ");
+                    int codigoDel = sc.nextInt();
+                    sc.nextLine();
+
+                    if (controlador.eliminarMascota(codigoDel)) {
+                        System.out.println("Mascota eliminada con exito!");
+                    } else {
+                        System.out.println("Mascota no encontrada!");
+                    }
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta, intente de nuevo.");
+            }
+        }
+    }
+
+    // ---------------- SUBMENU VETERINARIOS ----------------
+    public void menuVeterinarios() {
+        int opcion = -1;
+        while (opcion != 5) {
+            System.out.println("""
+                               -------------------------------------------------
+                                                VETERINARIOS
+                               1. AGREGAR
+                               2. MOSTRAR
+                               3. ACTUALIZAR
+                               4. ELIMINAR
+                               5. VOLVER
+                               SELECCIONE LA OPCION A REALIZAR:
+                               """);
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingresa el codigo: ");
+                    int codigo = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Nombre del veterinario: ");
+                    String nombre = sc.nextLine();
+                    System.out.print("Especialidad: ");
+                    String especialidad = sc.nextLine();
+
+                    veterinario_model veterinario = new veterinario_model(codigo, nombre, especialidad);
+                    controlador.agregarVeterinario(veterinario);
+                    break;
+
+                case 2:
+                    controlador.listarVeterinarios();
+                    break;
+
+                case 3:
+                    System.out.print("Codigo del veterinario a actualizar: ");
+                    int codigoAct = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Nuevo nombre: ");
+                    String nombreAct = sc.nextLine();
+                    System.out.print("Nueva especialidad: ");
+                    String especialidadAct = sc.nextLine();
+
+                    if (controlador.actualizarVeterinario(codigoAct, nombreAct, especialidadAct)) {
+                        System.out.println("Veterinario actualizado exitosamente!");
+                    } else {
+                        System.out.println("Veterinario no encontrado!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Ingrese el codigo del veterinario a eliminar: ");
+                    int codigoDel = sc.nextInt();
+                    sc.nextLine();
+
+                    if (controlador.eliminarVeterinario(codigoDel)) {
+                        System.out.println("Veterinario eliminado con exito!");
+                    } else {
+                        System.out.println("Veterinario no encontrado!");
+                    }
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta, intente de nuevo.");
+            }
+        }
+    }
+
+   
