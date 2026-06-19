7cc038e1c4d7b90ecbd0bf55d8ddde14fe632288
Jairo <jean.serna2730@utc.edu.ec>
2026-06-19 15:23:10 -0500

    public

---
+++ src/Veterinariamvc/Vetererinariamvc.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Veterinariamvc;
+
+/**
+ *
+ * @author el_ingeniero
+ */
+public class Vetererinariamvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
