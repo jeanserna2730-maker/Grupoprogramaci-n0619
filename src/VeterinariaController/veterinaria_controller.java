876aa10ce514ab8222d44b8e81093d07fd0c8892
washingtonsislema9363-lgtm <washington.sislema9363@utc.edu.ec>
2026-06-19 11:53:13 -0500

    import arraylist

---
+++ src/VeterinariaController/veterinaria_controller.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VeterinariaController;
+import java.util.ArraysList;
+import VeterinariaModel.veterinaria_model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class veterinaria_controller {
+    
+    
+}
