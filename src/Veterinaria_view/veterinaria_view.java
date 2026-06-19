fb9a125f41567d68fec48a199c79dfe0a1f30af2
Jairo <jean.serna2730@utc.edu.ec>
2026-06-19 13:42:50 -0500

    menu

--- src/Veterinaria_view/veterinaria_view.java
+++ src/Veterinaria_view/veterinaria_view.java
@@ -1,20 +1,49 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Veterinaria_view;
 
-/**
- *
- * @author el_ingeniero
- */
-public class veterinaria_view {
+import java.util.Scanner;
+import VeterinariaController.veterinaria_controller;
+import VeterinariaModel.mascota_model;
+import VeterinariaModel.veterinario_model;
+import VeterinariaModel.cita_model;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class veterinaria_view {
+    Scanner sc = new Scanner(System.in);
+    veterinaria_controller controlador = new veterinaria_controller();
+
+    public void menu() {
+        int opcion = -1;
+
+        while (opcion != 4) {
+            System.out.println("""
+                               -------------------------------------------------
+                                            MENU PRINCIPAL VETERINARIA
+                               1. GESTIONAR MASCOTAS
+                               2. GESTIONAR VETERINARIOS
+                               3. GESTIONAR CITAS
+                               4. SALIR
+                               SELECCIONE LA OPCION A REALIZAR:
+                               """);
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    menuMascotas();
+                    break;
+                case 2:
+                    menuVeterinarios();
+                    break;
+                case 3:
+                    menuCitas();
+                    break;
+                case 4:
+                    System.out.println("Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta, intente de nuevo.");
+            }
+        }
     }
-    
-}
